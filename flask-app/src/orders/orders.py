f93a3ad1541435382d47df6fdc91e15a812da7fe
CharAzna <129983171+CharAzna@users.noreply.github.com>
2023-04-14 21:53:42 -0400

    /Get API
    Signed-off-by: CharAzna <129983171+CharAzna@users.noreply.github.com>

--- flask-app/src/orders/orders.py
+++ flask-app/src/orders/orders.py
@@ -35,3 +35,30 @@ def get_orders():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+  
+  # Get all the orders
+@orders.route('/orders/6', methods=['GET'])
+def get_orders_6():
+  # get a cursor object from the database
+  cursor = db.get_db().cursor()
+
+  # use cursor to query the database for a list of products
+  cursor.execute('SELECT * FROM Orders WHERE restaurant_id = 6 ORDER BY order_time ASC')
+
+  # grab the column headers from the returned data
+  column_headers = [x[0] for x in cursor.description]
+
+  # create an empty dictionary object to use in 
+  # putting column headers together with data
+  json_data = []
+
+  # fetch all the data from the cursor
+  theData = cursor.fetchall()
+
+  # for each of the rows, zip the data elements together with
+  # the column headers. 
+  for row in theData:
+      json_data.append(dict(zip(column_headers, row)))
+
+  return jsonify(json_data)
