65e9075ac8e2b6e7d631f6c9fb35a2c172c83b67
CharAzna <129983171+CharAzna@users.noreply.github.com>
2023-04-17 17:17:53 -0400

    /burger_express/menu
    Return the menu of Burger Express
    
    Signed-off-by: CharAzna <129983171+CharAzna@users.noreply.github.com>

--- flask-app/src/customer_ben_blueprint/customer_ben_blueprint.py
+++ flask-app/src/customer_ben_blueprint/customer_ben_blueprint.py
@@ -115,3 +115,37 @@ def get_restaurants_newyork_discountfood():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+  
+  # [Ben-4]
+# Get the menu of Burger Express from the dataabase
+@customer_ben_blueprint.route('/burger_express/menu', methods=['GET'])
+def get_burgerexpress_menu():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for the menu of Burger Express
+    cursor.execute(
+      '''SELECT 
+      Foods.name as \"Name\", 
+      Foods.price as \"Price\"
+      FROM Restaurants
+      JOIN Menus ON Restaurants.restaurant_id = Menus.restaurant_id
+      JOIN Foods ON Menus.menu_id = Foods.menu_id
+      WHERE Restaurants.name = \'Burger Express\'''')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
