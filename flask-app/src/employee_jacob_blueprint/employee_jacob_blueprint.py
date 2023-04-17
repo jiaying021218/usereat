a34a6f397c7c7f1c03b9107ef5811332a58cd0c8
Jiaying Zheng <122384396+jiaying021218@users.noreply.github.com>
2023-04-17 16:10:03 -0400

    Update Query
    Update get orders query
    
    Signed-off-by: Jiaying Zheng <122384396+jiaying021218@users.noreply.github.com>

--- flask-app/src/employee_jacob_blueprint/employee_jacob_blueprint.py
+++ flask-app/src/employee_jacob_blueprint/employee_jacob_blueprint.py
@@ -11,14 +11,24 @@ def test_route():
   return "<h1>This is a test for employee Jacob</h1>"
 
 # [Jacob-1]
-# Get all the orders of restaurant with Id 6
+# Get all the orders for Burger Express at NY
 @employee_jacob_blueprint.route('/orders', methods=['GET'])
 def get_orders():
   # get a cursor object from the database
   cursor = db.get_db().cursor()
 
-  # use cursor to query the database for a list of orders
-  cursor.execute('SELECT * FROM Orders WHERE restaurant_id = 6 ORDER BY order_time ASC')
+  # use cursor to query the database for a list of orders for Burger Express at NY
+  cursor.execute(
+    '''SELECT 
+    Orders.order_id as \"Order ID\", 
+    Orders.order_time as \"Order Time\", 
+    Orders.order_status as \"Status\", 
+    Foods.name as \"Food Name\", 
+    Order_Items.quantity as \"Quantity\" 
+    FROM Orders
+    JOIN Order_Items ON Orders.order_id = Order_Items.order_id
+    JOIN Foods ON Order_Items.food_id = Foods.food_id
+    WHERE Orders.restaurant_location_id = 11''')
 
   # grab the column headers from the returned data
   column_headers = [x[0] for x in cursor.description]
