018db6ed56c86450c4700488d7c8927d3017aae9
Jiaying Zheng <122384396+jiaying021218@users.noreply.github.com>
2023-04-17 15:10:30 -0400

    Add Route
    Added new route for discount New York food
    
    Signed-off-by: Jiaying Zheng <122384396+jiaying021218@users.noreply.github.com>

--- flask-app/src/customer_ben_blueprint/customer_ben_blueprint.py
+++ flask-app/src/customer_ben_blueprint/customer_ben_blueprint.py
@@ -18,7 +18,7 @@ def get_restaurants_newyork():
 
     # use cursor to query the database for a list of New York restaurants
     cursor.execute(
-      '''SELECT * 
+      '''SELECT Restaurants.name as \"Restaurant Name\", Restaurants_Locations.address as \"Address\", Restaurants_Locations.city as \"City\", Restaurants_Locations.state as \"State\", Restaurants_Locations.zip as \"Zip\"
       FROM Restaurants JOIN Restaurants_Locations ON Restaurants.restaurant_id = Restaurants_Locations.restaurant_id 
       WHERE Restaurants_Locations.city = \'New York\'''')
 
@@ -47,11 +47,44 @@ def get_restaurants_newyork_fastfood():
 
     # use cursor to query the database for a list of New York fast food restaurants
     cursor.execute(
-      '''SELECT * 
+      '''SELECT Restaurants.name as \"Restaurant Name\", Restaurants_Locations.address as \"Address\", Restaurants_Locations.city as \"City\", Restaurants_Locations.state as \"State\", Restaurants_Locations.zip as \"Zip\"
       FROM Restaurants
       JOIN Restaurants_Locations ON Restaurants.restaurant_id = Restaurants_Locations.restaurant_id
       Join Categories ON Restaurants.category_id = Categories.category_id
       WHERE Restaurants_Locations.city = \'New York\' AND Categories.name = \'Fast Food\'''')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+  
+# Get all the New York discount food from the database
+@customer_ben_blueprint.route('/restaurants/new_york/discount_food', methods=['GET'])
+def get_restaurants_newyork_discountfood():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of New York discount food
+    cursor.execute(
+      '''SELECT Foods.name as \"Name\", (Foods.price - Foods.discount) as \"Discounted Price\", Restaurants.name as \"Restaurants Name\" 
+      FROM Restaurants
+      JOIN Restaurants_Locations ON Restaurants.restaurant_id = Restaurants_Locations.restaurant_id
+      JOIN Menus ON Restaurants.restaurant_id = Menus.restaurant_id
+      JOIN Foods ON Menus.menu_id = Foods.menu_id
+      WHERE Restaurants_Locations.city = \'New York\' AND Foods.discount > 0.00
+      ORDER BY Foods.price - Foods.discount ASC''')
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
