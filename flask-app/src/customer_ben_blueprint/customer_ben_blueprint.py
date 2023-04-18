76819a582ca326c27986fbcb589efda54c2cdbc2
Jiaying Zheng <122384396+jiaying021218@users.noreply.github.com>
2023-04-18 19:26:31 -0400

    Update Route
    Signed-off-by: Jiaying Zheng <122384396+jiaying021218@users.noreply.github.com>

--- flask-app/src/customer_ben_blueprint/customer_ben_blueprint.py
+++ flask-app/src/customer_ben_blueprint/customer_ben_blueprint.py
@@ -19,12 +19,16 @@ def get_restaurants_newyork():
 
     # use cursor to query the database for a list of New York restaurants
     cursor.execute(
-      '''SELECT Restaurants.name as \"Restaurant Name\", 
-      Restaurants_Locations.address as \"Address\", 
-      Restaurants_Locations.city as \"City\", 
-      Restaurants_Locations.state as \"State\", 
-      Restaurants_Locations.zip as \"Zip\"
-      FROM Restaurants JOIN Restaurants_Locations ON Restaurants.restaurant_id = Restaurants_Locations.restaurant_id 
+      '''SELECT 
+      Restaurants.name as "Restaurant Name", 
+      Restaurants_Locations.address as "Address", 
+      Restaurants_Locations.city as "City", 
+      Restaurants_Locations.state as "State", 
+      Restaurants_Locations.zip as "Zip",
+      Categories.name as "Category"
+      FROM Restaurants 
+      JOIN Restaurants_Locations ON Restaurants.restaurant_id = Restaurants_Locations.restaurant_id 
+      JOIN Categories ON Restaurants.category_id = Categories.category_id
       WHERE Restaurants_Locations.city = \'New York\'''')
 
     # grab the column headers from the returned data
@@ -45,23 +49,24 @@ def get_restaurants_newyork():
     return jsonify(json_data)
 
 # [Ben-2]
-# Get all the New York fast food restaurants from the database
-@customer_ben_blueprint.route('/restaurants/new_york/fast_food', methods=['GET'])
-def get_restaurants_newyork_fastfood():
+# Get all the New York restaurants from the database that belongs to the given category
+@customer_ben_blueprint.route('/restaurants/new_york/<int:category_id>', methods=['GET'])
+def get_restaurants_newyork_category(category_id):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
-    # use cursor to query the database for a list of New York fast food restaurants
+    # use cursor to query the database for a list of New York restaurants that belongs to the given category
     cursor.execute(
-      '''SELECT Restaurants.name as \"Restaurant Name\", 
-      Restaurants_Locations.address as \"Address\", 
-      Restaurants_Locations.city as \"City\", 
-      Restaurants_Locations.state as \"State\", 
-      Restaurants_Locations.zip as \"Zip\"
+      '''SELECT 
+      Restaurants.name as "Restaurant Name", 
+      Restaurants_Locations.address as "Address", 
+      Restaurants_Locations.city as "City", 
+      Restaurants_Locations.state as "State", 
+      Restaurants_Locations.zip as "Zip"
       FROM Restaurants
       JOIN Restaurants_Locations ON Restaurants.restaurant_id = Restaurants_Locations.restaurant_id
       Join Categories ON Restaurants.category_id = Categories.category_id
-      WHERE Restaurants_Locations.city = \'New York\' AND Categories.name = \'Fast Food\'''')
+      WHERE Restaurants_Locations.city = \'New York\' AND Categories.category_id = %s''', category_id)
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
@@ -89,9 +94,10 @@ def get_restaurants_newyork_discountfood():
 
     # use cursor to query the database for a list of New York discount food
     cursor.execute(
-      '''SELECT Foods.name as \"Name\", 
-      (Foods.price - Foods.discount) as \"Discounted Price\", 
-      Restaurants.name as \"Restaurants Name\" 
+      '''SELECT 
+      Foods.name as "Name", 
+      (Foods.price - Foods.discount) as "Discounted Price", 
+      Restaurants.name as "Restaurants Name" 
       FROM Restaurants
       JOIN Restaurants_Locations ON Restaurants.restaurant_id = Restaurants_Locations.restaurant_id
       JOIN Menus ON Restaurants.restaurant_id = Menus.restaurant_id
@@ -119,20 +125,20 @@ def get_restaurants_newyork_discountfood():
 
 # [Ben-4]
 # Get the menu of Burger Express from the database
-@customer_ben_blueprint.route('/restaurants/<int:restaurant_id>/menu', methods=['GET'])
-def get_burgerexpress_menu(restaurant_id):
+@customer_ben_blueprint.route('/menus/<int:menu_id>', methods=['GET'])
+def get_menu_id(menu_id):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
     # use cursor to query the database for the menu of Burger Express
     cursor.execute(
       '''SELECT 
-      Foods.name as \"Name\", 
-      Foods.price as \"Price\"
+      Foods.name as "Name", 
+      Foods.price as "Price"
       FROM Restaurants
       JOIN Menus ON Restaurants.restaurant_id = Menus.restaurant_id
       JOIN Foods ON Menus.menu_id = Foods.menu_id
-      WHERE Restaurants.restaurant_id = %s''', restaurant_id)
+      WHERE Menus.menu_id = %s''', menu_id)
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
@@ -160,11 +166,13 @@ def get_customer_id_orders(customer_id):
 
     # use cursor to query the database for a list of orders placed by the customer
     cursor.execute(
-        '''SELECT Orders.order_id as "Order ID",
+        '''SELECT 
+        Orders.order_id as "Order ID",
         Orders.order_time as "Order Time",
         Orders.order_status as "Order Status",
         Orders.restaurant_location_id as "Restaurant Location ID",
         Orders.restaurant_id as "Restaurant ID",
+        Orders.customer_id as "Customer ID",
         Orders.customer_address_id as "Customer Address ID",
         Orders.ETA as "ETA",
         Orders.price as "Total Price"
